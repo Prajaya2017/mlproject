d5cab24983968288656faad25afe16b0c698d445
prajaya2009@gmail.com <prajaya2009@gmail.com>
2026-01-18 23:03:59 -0700

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,29 +1,39 @@
 import sys
-import logging
+from src.logger import logging
 
-def error_message_detail(error, error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info()
-    file_name =exc_tb.tbframe.f_code.co_filename
-    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-    file_name,exc_tb.tb_lineno,str(error))
+
+def error_message_detail(error, error_detail):
+    _, _, exc_tb = error_detail.exc_info()
+
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+
+    error_message = (
+        f"Error occurred in python script name [{file_name}] "
+        f"line number [{line_number}] "
+        f"error message [{error}]"
+    )
 
     return error_message
-    
+
 
 class CustomException(Exception):
-    def __init__(self, error_message,error_detail:sys):
+    def __init__(self, error_message, error_detail):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail = error_detail)
-
+        self.error_message = error_message_detail(
+            error_message, error_detail
+        )
 
     def __str__(self):
         return self.error_message
-    
-# if __name__=="__main__":
-#         try:
-#             a =1/0
-#         except Exception as e:
-#             logging.info('Divide by Zero')
-#             raise CustomException(e,sys)
+
+
+# if __name__ == "__main__":
+#     try:
+#         a = 1 / 0
+#     except Exception as e:
+#         logging.info("Divide by Zero")
+#         raise CustomException(e, sys)
+
         
 
